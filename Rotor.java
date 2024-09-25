02ef36d4c5bfc0b2d069f849f7ed38ae9f52d403
Ayushi Kriplani <ayushikriplani@Ayushis-MacBook-Pro.local>
2024-09-25 16:38:59 -0400

    Added to Rotor file

--- Rotor.java
+++ Rotor.java
@@ -14,6 +14,9 @@ public class Rotor {
     
     public boolean rotate(){
         //String to char array 
+
+        rotorValues input = new rotorValues (); 
+        
         //char array at the last index move to the top 
         //loop through and do array[i] = array [i+1]
         //boolean check if it equals the current character 
