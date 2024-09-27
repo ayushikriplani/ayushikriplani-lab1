4e7d7c23c5745322ab27d585eb02c974c5af6d90
Ayushi Kriplani <ayushikriplani@Ayushis-MacBook-Pro.local>
2024-09-27 19:12:48 -0400

    Implemented Enigma Logic

--- Enigma.java
+++ Enigma.java
@@ -20,15 +20,45 @@ public class Enigma{
 
 
     public String decrypt(String message){        
-        //TODO
+        char [] convert = message.toCharArray(); 
+        char [] decryptArray = new char[message.length()]; 
+        for(int i = 0; i<convert.length; i++){
+            int step1 = rotors[2].indexOf(convert[i]); 
+            char step2 = rotors[1].charAt(step1); 
+            int step3 = rotors[2].indexOf(step2); 
+            char step4 = rotors[0].charAt(step3); 
+            decryptArray[i]= step4; 
+            rotate();
+       }
+       String decryptString = decryptArray.toString(); 
+       return decryptString; 
+
     }
 
 
     
     public String encrypt(String message){
-        //TODO
+       char [] convert = message.toCharArray(); 
+       char [] encryptArray = new char[message.length()]; 
+       for(int i = 0; i<convert.length; i++){
+            int step1 = rotors[0].indexOf(convert[i]); 
+            char step2 = rotors[2].charAt(step1); 
+            int step3 = rotors[1].indexOf(step2); 
+            char step4 = rotors[2].charAt(step3); 
+            encryptArray[i]= step4; 
+            rotate();
+       }
+       String encryptString = encryptArray.toString(); 
+       return encryptString; 
 
-    
+        //get message at index 0 
+        // find message in inner ring 
+        //same index at outer router 
+        //find outer router letter 
+        //find same letter in middle row
+        //same index on outer ring 
+
+    }
     private void rotate(){
         if(rotors[0].rotate()){
             if(rotors[1].rotate()){
@@ -36,5 +66,4 @@ public class Enigma{
             }
         }
     }
-    
 }
