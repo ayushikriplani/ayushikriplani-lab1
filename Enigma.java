d8d59fc58fa3f82613cabb156c4c59d9d0d95c32
ayushikriplani <ayushikriplani@gwu.edu>
2024-09-27 22:11:34 -0400

    Added Enigma Logic

--- Enigma.java
+++ Enigma.java
@@ -19,16 +19,33 @@ public class Enigma{
     }
 
 
-    public String decrypt(String message){        
-        //TODO
+    public String decrypt(String message){     
+        String decryptedString = new String ("");  
+        for (int i = 0; i< message.length(); i++){
+            int step1 = rotors[2].indexOf(message.charAt(i)); 
+            char step2 = rotors[1].charAt(step1); 
+            int step3 = rotors[2].indexOf(step2); 
+            char step4 = rotors[0].charAt(step3);
+            decryptedString = decryptedString + step4; 
+            rotate(); 
+        }
+        return decryptedString; 
     }
 
 
     
     public String encrypt(String message){
-        //TODO
-
-    
+        String encryptedString = new String ("");
+        for (int i = 0; i< message.length(); i++){
+            int step1 = rotors[0].indexOf(message.charAt(i)); 
+            char step2 = rotors[2].charAt(step1); 
+            int step3 = rotors[1].indexOf(step2); 
+            char step4 = rotors[2].charAt(step3);
+            encryptedString = encryptedString + step4; 
+            rotate(); 
+        }
+        return encryptedString; 
+    }
     private void rotate(){
         if(rotors[0].rotate()){
             if(rotors[1].rotate()){
